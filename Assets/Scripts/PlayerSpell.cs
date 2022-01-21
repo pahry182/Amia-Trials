bb97696afb92ab31036c8181dd004104ee0b6c48
rauf <pahry182@gmail.com>
2022-01-22 00:05:05 +0700

    update

---
+++ Assets/Scripts/PlayerSpell.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+`
+public class PlayerSpell : MonoBehaviour
+{
+    private UnitBase _ub;
+
+    private void Awake()
+    {
+        _ub.GetComponent<UnitBase>();
+    }
+
+
+}
